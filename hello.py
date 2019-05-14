ad53f3664eb713991013382617c7780ab45a6fd7
Aaron Helton <aaronhelton@Aarons-MBP.hsd1.va.comcast.net>
2019-05-14 19:53:14 -0400

    made some changes to the for loop

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-import sys
+cars = ["audi", "bmw", "subaru", "toyota"]
 
+for car in cars:
+    if car == "bmw":
+        print(car.upper())
+    else:
+        print(car.title())
 
-print("something new")
